0a86aaefd0474822239a5e3481e5221a73399710
titusergio <sergiogras99@gmail.com>
2021-04-08 19:13:22 +0200

    primera

---
+++ src/main/ProductManager.java
@@ -0,0 +1,17 @@
+package main;
+
+import Clases.Producte;
+import Clases.Comanda;
+import Clases.Usuari;
+
+import java.util.List;
+
+
+public interface ProductManager {
+
+    List<Producte> getProductePreu();
+
+
+
+
+}
